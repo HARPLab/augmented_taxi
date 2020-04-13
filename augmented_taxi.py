84c62ffe2f665455e969be7fa27ef572b6cdb443
Mike Lee <symikelee@gmail.com>
2020-04-13 10:14:25 -0400

    Refactor augmented_taxi.py and plot all historical priors in bayesian_IRL.py (used for visualization and debugging) on the same graph.

--- augmented_taxi.py
+++ augmented_taxi.py
@@ -42,24 +42,7 @@ def generate_env(env_code):
 
     return requested_passenger, requested_tolls
 
-def obtain_summary(vi_name):
-    # Augmented Taxi details
-    agent_a = {"x": 4, "y": 1, "has_passenger": 0}
-    walls_a = [{"x": 1, "y": 3, "fee": 1}, {"x": 1, "y": 2, "fee": 1}]
-    traffic_a = [] # probability that you're stuck
-    fuel_station_a = []
-    gamma_a = 0.95
-    width_a = 4
-    height_a = 3
-
-    # (on the goal with the passenger, on a toll
-    weights = np.array([[0.5, -0.075]])
-    weights_lb = np.array([-3., -1.])
-    weights_ub = np.array([3., 1.])
-
-    # a) single environment with a single weight
-    passengers_a = [{"x": 4, "y": 1, "dest_x": 1, "dest_y": 1, "in_taxi": 0}]
-    tolls_a = [{"x": 3, "y": 1, "fee": 1}, {"x": 3, "y": 2, "fee": 1}]
+def generate_agent(agent_a, walls_a, traffic_a, fuel_station_a, passengers_a, tolls_a, gamma_a, width_a, height_a, weights, vi_name, visualize=False):
 
     try:
         with open('models/vi_{}_agent.pickle'.format(vi_name), 'rb') as f:
@@ -75,12 +58,13 @@ def obtain_summary(vi_name):
             pickle.dump((mdp_agent, vi_agent), f)
 
     # Visualize agent
-    # fixed_agent = FixedPolicyAgent(vi_agent.policy)
-    # mdp_agent.visualize_agent(fixed_agent)
-    # mdp.reset()  # reset the current state to the initial state
-    # mdp.visualize_interaction()
+    if visualize:
+        fixed_agent = FixedPolicyAgent(vi_agent.policy)
+        mdp_agent.visualize_agent(fixed_agent)
+        # mdp.reset()  # reset the current state to the initial state
+        # mdp.visualize_interaction()
 
-    # b) enumeration of all possible optimal policies from possible environments x weight candidates
+def obtain_summary(agent_a, walls_a, traffic_a, fuel_station_a, gamma_a, width_a, height_a, weights, vi_name):
     # obtain uniformly the discretized reward weight candidates
     n_wt_partitions = 2
     try:
@@ -112,6 +96,7 @@ def obtain_summary(vi_name):
         wt_vi_traj_candidates = []
         n_processed_envs = 0
 
+    # enumeration of all possible optimal policies from possible environments x weight candidates
     # if there are environments and weights yet to be processed
     if n_processed_envs < len(env_codes):
         for env_idx in range(n_processed_envs, len(env_codes)):
@@ -126,15 +111,7 @@ def obtain_summary(vi_name):
                                                fuel_stations=fuel_station_a, gamma=gamma_a, weights=wt_candidate)
                 vi_candidate = ValueIteration(mdp_candidate, sample_rate=1)
                 iterations, value_of_init_state = vi_candidate.run_vi()
-
-                # if np.ndarray.tolist(wt_candidate) == np.ndarray.tolist(weights): # todo: debugging
-                #     fixed_agent = FixedPolicyAgent(vi_candidate.policy)
-                #     mdp_candidate.visualize_agent(fixed_agent)
-
                 trajectory = mdp_helpers.rollout_policy(mdp_candidate, vi_candidate)
-                # if np.ndarray.tolist(wt_candidate) == np.ndarray.tolist(weights): # todo: debugging
-                #     mdp_candidate.visualize_trajectory(trajectory)
-
                 wt_vi_traj_env.append((wt_candidate, vi_candidate, trajectory))
 
                 wt_counter += 1
@@ -163,19 +140,19 @@ def obtain_summary(vi_name):
 
     # compute the Bayesian IRL-based policy summary
     n_demonstrations = 10
-    bayesian_IRL_summary = bayesian_IRL.obtain_summary(n_demonstrations, weights, wt_uniform_sampling, wt_vi_traj_candidates, visualize=True)
+    bayesian_IRL_summary, tracking_priors = bayesian_IRL.obtain_summary(n_demonstrations, weights, wt_uniform_sampling, wt_vi_traj_candidates, approximate=True, visualize=True)
 
     for policy_traj_tuple in bayesian_IRL_summary:
         mdp_demo = policy_traj_tuple[0].mdp
         mdp_demo.visualize_trajectory(policy_traj_tuple[1])
 
     with open('models/bayesian_IRL_demos_{}.pickle'.format(vi_name), 'wb') as f:
-        pickle.dump(bayesian_IRL_summary, f)
+        pickle.dump((bayesian_IRL_summary, tracking_priors), f)
 
 
 def replay_summary(vi_name):
     with open('models/bayesian_IRL_demos_{}.pickle'.format(vi_name), 'rb') as f:
-        bayesian_IRL_summary = pickle.load(f)
+        bayesian_IRL_summary, tracking_priors = pickle.load(f)
 
     for policy_traj_tuple in bayesian_IRL_summary:
         mdp_demo = policy_traj_tuple[0].mdp
@@ -184,5 +161,24 @@ def replay_summary(vi_name):
 
 if __name__ == "__main__":
     vi_name = 'feature-based'
-    obtain_summary(vi_name)
-    # replay_summary(vi_name)
+
+    # Augmented Taxi details
+    agent_a = {"x": 4, "y": 1, "has_passenger": 0}
+    walls_a = [{"x": 1, "y": 3, "fee": 1}, {"x": 1, "y": 2, "fee": 1}]
+    traffic_a = [] # probability that you're stuck
+    fuel_station_a = []
+    gamma_a = 0.95
+    width_a = 4
+    height_a = 3
+
+    passengers_a = [{"x": 4, "y": 1, "dest_x": 1, "dest_y": 1, "in_taxi": 0}]
+    tolls_a = [{"x": 3, "y": 1, "fee": 1}]
+
+    # (on the goal with the passenger, on a toll
+    weights = np.array([[0.5, -0.075]])
+    weights_lb = np.array([-3., -1.])
+    weights_ub = np.array([3., 1.])
+
+    generate_agent(agent_a, walls_a, traffic_a, fuel_station_a, passengers_a, tolls_a, gamma_a, width_a, height_a, weights, vi_name, visualize=True)
+    obtain_summary(agent_a, walls_a, traffic_a, fuel_station_a, gamma_a, width_a, height_a, weights, vi_name)
+    replay_summary(vi_name)
--- policy_summarization/bayesian_IRL.py
+++ policy_summarization/bayesian_IRL.py
@@ -2,6 +2,7 @@
 import matplotlib.pyplot as plt
 from termcolor import colored
 import numpy as np
+from itertools import chain
 
 # Other imports
 from simple_rl.agents import FixedPolicyAgent
@@ -20,14 +21,14 @@ Returns:
 Summary:
     An implementation of 'Enabling Robots to Communicate their Objectives' (Huang et al. AURO2019).
 '''
-def obtain_summary(n_demonstrations, weights, wt_uniform_sampling, wt_vi_traj_candidates, visualize=False):
+def obtain_summary(n_demonstrations, weights, wt_uniform_sampling, wt_vi_traj_candidates, approximate=True, visualize=False):
     priors = {}                # up-to-date priors on candidates
-    debugging_priors = {}      # a history of updated priors for debugging
+    history_priors = {}        # a history of updated priors for debugging
 
     # initialize the prior to be a uniform distribution
     for wt_candidate in wt_uniform_sampling:
         priors[wt_candidate.tostring()] = 1. / len(wt_uniform_sampling)
-        debugging_priors[wt_candidate.tostring()] = [1. / len(wt_uniform_sampling)]
+        history_priors[wt_candidate.tostring()] = [1. / len(wt_uniform_sampling)]
 
     IRL_summary = []
     update_coeff = 0.01 # 10^-5 to 10^5 used by Huang et al. for approximate inference
@@ -51,19 +52,20 @@ def obtain_summary(n_demonstrations, weights, wt_uniform_sampling, wt_vi_traj_ca
                 vi_candidate = wt_vi_traj_candidates_tuple[1]
                 trajectory_candidate = wt_vi_traj_candidates_tuple[2]
 
-                # a) exact inference IRL
-                # reward_diff = wt_candidate.dot(vi_candidate.mdp.accumulate_reward_features(trajectory).T) - wt_candidate.dot(vi_candidate.mdp.accumulate_reward_features(trajectory_candidate).T)
-                # if reward_diff >= 0:
-                #     cond_trajectory_likelihood = 1
-                # else:
-                #     cond_trajectory_likelihood = 0
-
-                # b) approximate inference IRL
-                # take the abs value in case you're working with partial trajectories, in which the comparative rewards
-                # for short term behavior differs from comparative rewards for long term behavior
-                reward_diff = abs((wt_candidate.dot(vi_candidate.mdp.accumulate_reward_features(trajectory_candidate).T) \
-                               - wt_candidate.dot(vi_candidate.mdp.accumulate_reward_features(trajectory).T))[0][0])
-                cond_trajectory_likelihood = np.exp(-update_coeff * reward_diff)
+                if approximate is False:
+                    # a) exact inference IRL
+                    reward_diff = wt_candidate.dot(vi_candidate.mdp.accumulate_reward_features(trajectory).T) - wt_candidate.dot(vi_candidate.mdp.accumulate_reward_features(trajectory_candidate).T)
+                    if reward_diff >= 0:
+                        cond_trajectory_likelihood = 1
+                    else:
+                        cond_trajectory_likelihood = 0
+                else:
+                    # b) approximate inference IRL
+                    # take the abs value in case you're working with partial trajectories, in which the comparative rewards
+                    # for short term behavior differs from comparative rewards for long term behavior
+                    reward_diff = abs((wt_candidate.dot(vi_candidate.mdp.accumulate_reward_features(trajectory_candidate).T) \
+                                   - wt_candidate.dot(vi_candidate.mdp.accumulate_reward_features(trajectory).T))[0][0])
+                    cond_trajectory_likelihood = np.exp(-update_coeff * reward_diff)
 
                 cond_trajectory_likelihoods[wt_candidate.tostring()] = cond_trajectory_likelihood
 
@@ -72,21 +74,22 @@ def obtain_summary(n_demonstrations, weights, wt_uniform_sampling, wt_vi_traj_ca
 
             cond_trajectory_likelihoods_trajectories.append(cond_trajectory_likelihoods)
 
-            # the reward diff should always be zero, assuming that the agent and the human are using the same reward
-            # features and cond_trajectory_likelihood_star should always be 1. you can use the most recent vi_candidate's
-            # MDP since the MDP should be shared by all within the same wt_vi_traj_candidate_tuples
-            # a) exact inference IRL
-            # reward_diff_star = (weights.dot(vi_candidate.mdp.accumulate_reward_features(trajectory).T) - \
-            #                    weights.dot(vi_candidate.mdp.accumulate_reward_features(trajectory).T))[0][0]
-            # if reward_diff_star >= 0:
-            #     cond_trajectory_likelihood_star = 1
-            # else:
-            #     cond_trajectory_likelihood_star = 0
-
-            # b) approximate inference IRL
-            reward_diff_star = abs((weights.dot(vi_candidate.mdp.accumulate_reward_features(trajectory).T) - \
-                               weights.dot(vi_candidate.mdp.accumulate_reward_features(trajectory).T))[0][0])
-            cond_trajectory_likelihood_star = np.exp(-update_coeff * reward_diff_star)
+            if approximate is False:
+                # the reward diff should always be zero, assuming that the agent and the human are using the same reward
+                # features and cond_trajectory_likelihood_star should always be 1. you can use the most recent vi_candidate's
+                # MDP since the MDP should be shared by all within the same wt_vi_traj_candidate_tuples
+                # a) exact inference IRL
+                reward_diff_star = (weights.dot(vi_candidate.mdp.accumulate_reward_features(trajectory).T) - \
+                                   weights.dot(vi_candidate.mdp.accumulate_reward_features(trajectory).T))[0][0]
+                if reward_diff_star >= 0:
+                    cond_trajectory_likelihood_star = 1
+                else:
+                    cond_trajectory_likelihood_star = 0
+            else:
+                # b) approximate inference IRL
+                reward_diff_star = abs((weights.dot(vi_candidate.mdp.accumulate_reward_features(trajectory).T) - \
+                                   weights.dot(vi_candidate.mdp.accumulate_reward_features(trajectory).T))[0][0])
+                cond_trajectory_likelihood_star = np.exp(-update_coeff * reward_diff_star)
 
             # calculate what the new condition probability of the true weight vector would be given this demonstration
             cond_posteriors[k] = 1. / Z * cond_trajectory_likelihood_star * priors[
@@ -112,7 +115,7 @@ def obtain_summary(n_demonstrations, weights, wt_uniform_sampling, wt_vi_traj_ca
         for wt_candidate in wt_uniform_sampling:
             normalized_prior = priors[wt_candidate.tostring()] / prior_sum
             priors[wt_candidate.tostring()] = normalized_prior
-            debugging_priors[wt_candidate.tostring()].append(normalized_prior)
+            history_priors[wt_candidate.tostring()].append(normalized_prior)
 
             if np.array_equal(wt_candidate, weights[0]):
                 print(colored('wt: {}, updated_prior: {}'.format(np.round(wt_candidate, 3), normalized_prior), 'red'))
@@ -123,14 +126,21 @@ def obtain_summary(n_demonstrations, weights, wt_uniform_sampling, wt_vi_traj_ca
 
     if visualize:
         # visualize the evolution of the prior distribution with each new demonstration
-        for j in range(len(IRL_summary)):
-            x = range(len(wt_uniform_sampling))
-            y = []
-            for wt_candidate in wt_uniform_sampling:
-                y.append(debugging_priors[wt_candidate.tostring()][j])
-            plt.plot(x, y)
-            plt.xlabel('Candidate reward weight vectors')
-            plt.ylabel('Probability of candidates')
-            plt.show()
+        history_priors_per_demo = []
+        x = range(len(wt_uniform_sampling))
 
-    return IRL_summary
+        # group the priors by demo, and not by weight
+        for j in range(len(IRL_summary)):
+            priors_per_wt_candidate = []
+            for wt_candidate in wt_uniform_sampling:
+                priors_per_wt_candidate.append(history_priors[wt_candidate.tostring()][j])
+            history_priors_per_demo.append(priors_per_wt_candidate)
+
+        # flatten the list of (x, history_priors_per_demo) tuples
+        plt.plot(*list(chain.from_iterable([(x, history_priors_per_demo[j]) for j in range(len(history_priors_per_demo))])))
+        plt.xlabel('Candidate reward weight vectors')
+        plt.ylabel('Probability of candidates')
+        plt.legend(['{}'.format(x) for x in range(len(IRL_summary))])
+        plt.show()
+
+    return IRL_summary, history_priors
