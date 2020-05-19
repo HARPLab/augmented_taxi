c5eec3117372a9dd04dc658564bd1dbddc6296c9
Mike Lee <symikelee@gmail.com>
2020-05-19 13:59:41 -0400

    Select test environments that maximize the disagreement between the optimal demonstrations of sampled test weight candidates and the ground truth weight.

--- augmented_taxi.py
+++ augmented_taxi.py
@@ -41,8 +41,8 @@ def obtain_BIRL_summary(data_loc, eval_fn, n_env, weights, weights_lb, weights_u
         with open('models/' + data_loc + '/BIRL_summary_{}.pickle'.format(eval_fn), 'rb') as f:
             bayesian_IRL_summary, wt_candidates, history_priors = pickle.load(f)
     except:
-        wt_candidates = ps_helpers.obtain_wt_candidates(data_loc, weights, weights_lb, weights_ub, step_cost_flag, n_wt_partitions=n_wt_partitions, iter_idx=iter_idx)
-        wt_vi_traj_candidates = ps_helpers.obtain_env_policies(data_loc, n_env, wt_candidates, agent_a, walls_a, traffic_a, fuel_station_a, gamma_a, width_a, height_a)
+        wt_candidates = ps_helpers.discretize_wt_candidates(data_loc, weights, weights_lb, weights_ub, step_cost_flag, n_wt_partitions=n_wt_partitions, iter_idx=iter_idx)
+        wt_vi_traj_candidates = ps_helpers.obtain_env_policies(data_loc, n_env, wt_candidates, agent_a, walls_a, traffic_a, fuel_station_a, gamma_a, width_a, height_a, 'BIRL')
         bayesian_IRL_summary, wt_candidates, history_priors = bayesian_IRL.obtain_summary(n_demonstrations, weights, wt_candidates, wt_vi_traj_candidates, eval_fn)
 
         with open('models/' + data_loc + '/BIRL_summary_{}.pickle'.format(eval_fn), 'wb') as f:
@@ -61,7 +61,7 @@ def obtain_BEC_summary(data_loc, n_env, weights, step_cost_flag, visualize_const
         with open('models/' + data_loc + '/BEC_constraints.pickle', 'rb') as f:
             constraints = pickle.load(f)
     except:
-        wt_vi_traj_candidates = ps_helpers.obtain_env_policies(data_loc, n_env, np.expand_dims(weights, axis=0), agent_a, walls_a, traffic_a, fuel_station_a, gamma_a, width_a, height_a)
+        wt_vi_traj_candidates = ps_helpers.obtain_env_policies(data_loc, n_env, np.expand_dims(weights, axis=0), agent_a, walls_a, traffic_a, fuel_station_a, gamma_a, width_a, height_a, 'ground_truth')
         try:
             with open('models/' + data_loc + '/BEC_constraints.pickle', 'rb') as f:
                 constraints = pickle.load(f)
@@ -82,6 +82,13 @@ def obtain_BEC_summary(data_loc, n_env, weights, step_cost_flag, visualize_const
 
     return constraints, BEC_summary
 
+def obtain_test_environments(data_loc, weights, n_env, n_samples, sample_radius, n_desired_test_env, step_cost_flag):
+    # obtain test weight candidates
+    wt_candidates = ps_helpers.sample_wt_candidates(data_loc, weights, step_cost_flag, n_samples, sample_radius)
+
+    # use test weight candidates to select test environments
+    test_wt_vi_traj_tuples = ps_helpers.obtain_test_environments(data_loc, weights, wt_candidates, n_env, n_desired_test_env, agent_a, walls_a, traffic_a, fuel_station_a, gamma_a, width_a, height_a, visualize=True)
+
 if __name__ == "__main__":
     # Augmented Taxi details (note that I'm allowing functions below to directly access these taxi variables w/o passing them in)
     agent_a = {"x": 4, "y": 1, "has_passenger": 0}
@@ -93,23 +100,29 @@ if __name__ == "__main__":
     width_a = 4
     height_a = 3
 
-    # (on the goal with the passenger, on a toll, step cost). assume the L2 norm of the weights is equal 1. WLOG
+    # reward weight parameters (on the goal with the passenger, on a toll, step cost).
+    # assume the L2 norm of the weights is equal 1. WLOG
     weights_lb = np.array([-1., -1., -0.0350311])
     weights_ub = np.array([1., 1., -0.0350311])
-
     weights = np.array([[0.9908798, -0.13011553, -0.0350311]])
     gamma_a = 1.
     step_cost_flag = True   # indicates that the last weight element is a known step cost. code currently assumes a 2D
                             # weight vector if step_cost_flag = False, and a 3D weight vector if step_cost_flag = True
+
+    # test environment selection parameters
+    n_desired_test_env = 5       # number of desired test environments
+    sample_radius = 0.75         # radius around the ground truth weight from which you will uniformly sample from
+                                 # to obtain test weight candidates
+    n_samples = 5                # number of test weight candidates to sample
 
     # Joint BIRL and BEC parameters
     n_env = 512                  # number of environments to consider
                                  # tip: select so that np.log(n_env) / np.log(2) yields an int for predictable behavior
                                  # see ps_helpers.obtain_env_policies()
     # BIRL parameters
-    n_wt = 10                    # total number of weight candidates (including the ground truth)
+    n_wt = 1                    # total number of weight candidates (including the ground truth)
                                  # tip: select n_wt to such that n_wt_partitions is an int to ensure that the exact
-                                 # number of desired weight candidates is actually incorporated. see ps_helpers.obtain_wt_candidates()
+                                 # number of desired weight candidates is actually incorporated. see ps_helpers.discretize_wt_candidates()
                                  # also note that n_wt = n_wt_partitions ** (# of weights you're discretizing over) + 1
 
     iter_idx = None              # weight dimension to discretize over. If None, discretize uniformly over all dimensions
@@ -117,21 +130,24 @@ if __name__ == "__main__":
     n_demonstrations = 10        # total number of demonstrations sought, in order of decreasing effectiveness
 
     if iter_idx == None:
-        data_loc_BIRL = str(n_env) + '_env_' + str(n_wt) + '_wt_' + 'uniform'
+        data_loc_BIRL = str(n_env) + '_env/' + str(n_wt) + '_wt_' + 'uniform'
         if step_cost_flag:
             n_wt_partitions = int((n_wt - 1) ** (1.0 / (weights.shape[1] - 1)))
         else:
             n_wt_partitions = int((n_wt - 1) ** (1.0 / weights.shape[1]))
     else:
-        data_loc_BIRL = str(n_env) + '_env_' + str(n_wt) + '_wt_' + 'iter_idx_' + str(iter_idx)
+        data_loc_BIRL = str(n_env) + '_env/' + str(n_wt) + '_wt_' + 'iter_idx_' + str(iter_idx)
         n_wt_partitions = n_wt - 1
-    data_loc_BEC = str(n_env) + '_env_gt_wt'
+    data_loc = str(n_env) + '_env'
 
     # a) generate an agent if you want to explore the Augmented Taxi MDP
     # generate_agent('base', agent_a, walls_a, traffic_a, fuel_station_a, passengers_a, tolls_a, gamma_a, width_a, height_a, weights, visualize=True)
 
     # b) obtain a Bayesian IRL summary of the agent's policy
-    # bayesian_IRL_summary, wt_candidates, history_priors = obtain_BIRL_summary(data_loc_BIRL, eval_fn, n_env, weights, weights_lb, weights_ub, n_wt_partitions, iter_idx, step_cost_flag, visualize_history_priors=True, visualize_summary=True)
+    # bayesian_IRL_summary, wt_candidates, history_priors = obtain_BIRL_summary(data_loc_BIRL, eval_fn, n_env, weights, weights_lb, weights_ub, n_wt_partitions, iter_idx, step_cost_flag, visualize_history_priors=False, visualize_summary=True)
 
     # c) obtain a BEC summary of the agent's policy
-    constraints, BEC_summary = obtain_BEC_summary(data_loc_BEC, n_env, weights, step_cost_flag, visualize_constraints=True, visualize_summary=True)
+    # constraints, BEC_summary = obtain_BEC_summary(data_loc, n_env, weights, step_cost_flag, visualize_constraints=True, visualize_summary=True)
+
+    # d) obtain test environments
+    obtain_test_environments(data_loc, weights, n_env, n_samples, sample_radius, n_desired_test_env, step_cost_flag)
--- policy_summarization/policy_summarization_helpers.py
+++ policy_summarization/policy_summarization_helpers.py
@@ -2,14 +2,60 @@ import numpy as np
 import dill as pickle
 import itertools
 import shutil
+import random
+from termcolor import colored
 
 # Other imports
 from simple_rl.tasks import AugmentedTaxiOOMDP
 from simple_rl.planning import ValueIteration
 from simple_rl.utils import mdp_helpers
 
+def sample_wt_candidates(data_loc, weights, step_cost_flag, n_samples, sample_radius):
+    '''
+    :param data_loc: location to load and save data from
+    :param weights: ground truth weight
+    :param step_cost_flag: whether the final element of the reward weight vector is a step cost (and should not be estimated)
+    :param n_samples: desired number of test weight candidates
+    :param sample_radius: radius around the ground truth weight from which you will uniformly sample from
+    :return:
 
-def obtain_wt_candidates(data_loc, weights, weights_lb, weights_ub, step_cost_flag, n_wt_partitions=0, iter_idx=None):
+    Summary: Sample test weight candidates. The radius from which you sample will dictate how difficult difficult
+    the test environments will be
+    '''
+    try:
+        with open('models/' + data_loc + '/test_wt_candidates.pickle', 'rb') as f:
+            wt_candidates = pickle.load(f)
+    except:
+        d = weights.shape[1] - step_cost_flag
+        wt_candidates = np.zeros((n_samples, d))
+        count = 0
+
+        # Muller method for sampling from a d-dimensional ball
+        while count < n_samples:
+            u = np.random.normal(0, 1, d)  # an array of d normally distributed random variables
+            norm = np.sum(u**2) ** (0.5)
+            r = sample_radius * random.random() ** (1.0 / d)
+            x = r * u / norm
+            shifted = x + weights[0, 0:d]
+
+            # ensure that the samples aren't out of bounds
+            if (shifted > 1.).any() or (shifted < -1.).any():
+                continue
+            else:
+                wt_candidates[count, :] = shifted
+                count += 1
+
+        if step_cost_flag:
+            wt_candidates = np.hstack((wt_candidates, np.ones((n_samples, 1)) * weights[0, -1]))
+
+        wt_candidates = wt_candidates.reshape((wt_candidates.shape[0], 1, wt_candidates.shape[1]))
+
+        with open('models/' + data_loc + '/test_wt_candidates.pickle', 'wb') as f:
+            pickle.dump(wt_candidates, f)
+
+    return wt_candidates
+
+def discretize_wt_candidates(data_loc, weights, weights_lb, weights_ub, step_cost_flag, n_wt_partitions=0, iter_idx=None):
     '''
     Args:
         weights: Ground truth weights (numpy array)
@@ -85,20 +131,31 @@ def generate_env(env_code):
 
     return requested_passenger, requested_tolls
 
-def obtain_env_policies(data_loc, n_env, wt_candidates, agent_a, walls_a, traffic_a, fuel_station_a, gamma_a, width_a, height_a):
+def obtain_env_policies(data_loc, n_env, wt_candidates, agent_a, walls_a, traffic_a, fuel_station_a, gamma_a, width_a, height_a, save_type):
     '''
     Summary: come up with an optimal policy for each of the candidates
     '''
 
     # generate codes that govern passenger's initial position and status of the eight tolls in the 4x3 environment
+    # todo: can't n_env be recovered by the size of wt_candidates?
     env_codes = list(map(list, itertools.product([0, 1], repeat=int(np.log(n_env) / np.log(2)))))
 
     save_mark = 750
+    if save_type == 'ground_truth':
+        filename = 'models/' + data_loc + '/gt_wt_vi_traj_candidates.pickle'
+        backup_filename = 'models/' + data_loc + '/gt_wt_vi_traj_candidates_backup.pickle'
+    elif save_type == 'test':
+        filename = 'models/' + data_loc + '/test_wt_vi_traj_candidates.pickle'
+        backup_filename = 'models/' + data_loc + '/test_wt_vi_traj_candidates_backup.pickle'
+    else:
+        filename = 'models/' + data_loc + '/wt_vi_traj_candidates.pickle'
+        backup_filename = 'models/' + data_loc + '/wt_vi_traj_candidates_backup.pickle'
+
     try:
-        with open('models/' + data_loc + '/wt_vi_traj_candidates.pickle', 'rb') as f:
+        with open(filename, 'rb') as f:
             wt_vi_traj_candidates = pickle.load(f)
 
-        if len(wt_vi_traj_candidates) == len(env_codes) and len(env_codes[-1]) == len(wt_candidates):
+        if len(wt_vi_traj_candidates) == len(env_codes) and len(wt_vi_traj_candidates[-1]) == len(wt_candidates):
             # all environments and weights have been processed
             n_processed_envs = len(env_codes)
         else:
@@ -121,7 +178,8 @@ def obtain_env_policies(data_loc, n_env, wt_candidates, agent_a, walls_a, traffi
                 mdp_candidate = AugmentedTaxiOOMDP(width=width_a, height=height_a, agent=agent_a, walls=walls_a,
                                                passengers=passengers_a, tolls=tolls_a, traffic=traffic_a,
                                                fuel_stations=fuel_station_a, gamma=gamma_a, weights=wt_candidate)
-                vi_candidate = ValueIteration(mdp_candidate, sample_rate=1)
+                # parameters tailored to the 4x3 Augmented Taxi Domain
+                vi_candidate = ValueIteration(mdp_candidate, sample_rate=1, max_iterations=50)
                 iterations, value_of_init_state = vi_candidate.run_vi()
                 trajectory = mdp_helpers.rollout_policy(mdp_candidate, vi_candidate)
                 wt_vi_traj_env.append((wt_candidate, vi_candidate, trajectory))
@@ -135,18 +193,95 @@ def obtain_env_policies(data_loc, n_env, wt_candidates, agent_a, walls_a, traffi
             print('Finished analyzing environment {}'.format(n_processed_envs))
 
             if n_processed_envs % save_mark == 0:
-                with open('models/' + data_loc + '/wt_vi_traj_candidates.pickle', 'wb') as f:
+                with open(filename, 'wb') as f:
                     pickle.dump(wt_vi_traj_candidates, f)
 
                 # make a backup in case the overwriting in the code above fails
-                shutil.copy2('models/' + data_loc + '/wt_vi_traj_candidates.pickle', 'models/' + data_loc + '/wt_vi_traj_candidates_backup.pickle')
+                shutil.copy2(filename, backup_filename)
 
                 print("Saved!")
 
-        with open('models/' + data_loc + '/wt_vi_traj_candidates.pickle', 'wb') as f:
+        with open(filename, 'wb') as f:
             pickle.dump(wt_vi_traj_candidates, f)
 
         # make a backup in case the overwriting in the code above fails
-        shutil.copy2('models/' + data_loc + '/wt_vi_traj_candidates.pickle', 'models/' + data_loc + '/wt_vi_traj_candidates_backup.pickle')
+        shutil.copy2(filename, backup_filename)
 
     return wt_vi_traj_candidates
+
+def obtain_test_environments(data_loc, weights, wt_candidates, n_env, n_desired_test_env, agent_a, walls_a, traffic_a, fuel_station_a, gamma_a, width_a, height_a, visualize=False):
+    '''
+    Summary: Select the environments with the most disagreement between the optimal polices of the ground truth weight
+    and the test weights as the test environments
+    '''
+    # obtain optimal policies for each of the test candidate weights and the ground truth candidate weight. It's assumed
+    # that the environments appear in the same order for both the test and ground truth weights.
+    test_wt_vi_traj_candidates = obtain_env_policies(data_loc, n_env, wt_candidates, agent_a, walls_a, traffic_a, fuel_station_a, gamma_a, width_a, height_a, save_type='test')
+
+    gt_wt_vi_traj_candidates = obtain_env_policies(data_loc, n_env, np.expand_dims(weights, axis=0), agent_a,
+                                                           walls_a, traffic_a, fuel_station_a, gamma_a, width_a,
+                                                           height_a, 'ground_truth')
+
+    discrepancy = np.zeros(n_env)
+
+    # compare the difference in reward between optimal demonstrations of the candidate weights to the optimal
+    # demonstration of the ground truth weight in each environment
+    for env_idx in range(n_env):
+        gt_weight = gt_wt_vi_traj_candidates[env_idx][0][0]
+        gt_mdp = gt_wt_vi_traj_candidates[env_idx][0][1].mdp
+        gt_trajectory= gt_wt_vi_traj_candidates[env_idx][0][2]
+        test_wt_vi_traj_candidates_tuples = test_wt_vi_traj_candidates[env_idx]
+
+        reward_diff = 0
+
+        for test_wt_vi_traj_candidates_tuple in test_wt_vi_traj_candidates_tuples:
+            wt_candidate = test_wt_vi_traj_candidates_tuple[0]
+            vi_candidate = test_wt_vi_traj_candidates_tuple[1]
+            trajectory_candidate = test_wt_vi_traj_candidates_tuple[2]
+
+            # note that this is different from the reward comparison in BIRL (see bayesian_IRL.py). Euclidean distance
+            # between the trajectories could also be used instead
+            reward_diff += abs(
+                (wt_candidate.dot(vi_candidate.mdp.accumulate_reward_features(trajectory_candidate, discount=True).T) \
+                 - gt_weight.dot(gt_mdp.accumulate_reward_features(gt_trajectory, discount=True).T))[0][0])
+
+        discrepancy[env_idx] = reward_diff
+
+    print(discrepancy)
+    # sort from greatest discrepancy to least discrepancy, taking the first n_desired_test_env as the test environments
+    test_idxs = (-discrepancy).argsort()[:n_desired_test_env]
+    print(test_idxs)
+    print(discrepancy[test_idxs])
+
+    # visualize each test environment, and the optimal trajectories
+    if visualize:
+        test_env_count = 1
+        for test_idx in test_idxs:
+            print(colored('Visualizing test environment {}'.format(test_env_count), 'red'))
+
+            # visualize the ground truth weight trajectory
+            print(colored('Ground truth weight: {}'.format(gt_wt_vi_traj_candidates[test_idx][0][0]), 'blue'))
+            gt_trajectory = gt_wt_vi_traj_candidates[test_idx][0][2]
+            gt_wt_vi_traj_candidates[test_idx][0][1].mdp.visualize_trajectory(gt_trajectory)
+
+            # visualize the candidate weight trajectories
+            test_wt_vi_traj_candidates_tuples = test_wt_vi_traj_candidates[test_idx]
+            test_wt_count = 1
+            for test_wt_vi_traj_candidates_tuple in test_wt_vi_traj_candidates_tuples:
+                wt_candidate = test_wt_vi_traj_candidates_tuple[0]
+                vi_candidate = test_wt_vi_traj_candidates_tuple[1]
+                trajectory_candidate = test_wt_vi_traj_candidates_tuple[2]
+
+                print(colored('#{} candidate weight: {}'.format(test_wt_count, wt_candidate), 'blue'))
+                vi_candidate.mdp.visualize_trajectory(trajectory_candidate)
+
+                test_wt_count += 1
+            test_env_count += 1
+
+    # return a list of [gt_wt_vi_traj_candidates, test_wt_vi_traj_candidates] that correspond to the selected test
+    # environments
+    test_wt_vi_traj_tuples = []
+    for test_idx in test_idxs:
+        test_wt_vi_traj_tuples.append([gt_wt_vi_traj_candidates[test_idx], test_wt_vi_traj_candidates[test_idx]])
+
+    return test_wt_vi_traj_tuples
