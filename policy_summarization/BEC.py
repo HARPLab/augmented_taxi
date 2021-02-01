Assume the human's belief of agent's weights starts off in the correct quadrant, and fix the sign of step cost in the human's belief. Accommodate Augmented Taxi and Skateboard domains having multiple viable exit states.

policy_summarization/BEC.py
@@ -236,7 +236,7 @@ def obtain_summary_counterfactual(summary_variant, wt_vi_traj_candidates, min_BE
 
     # c) assuming a starting human model without any inducing demonstrations. include a dummy constraint that doesn't
     # reduce any BEC area so that the BEC area calculation doesn't break later
-    min_BEC_constraints_running = [np.array([[.01, 0, -1]])]
+    min_BEC_constraints_running = [np.array([[0, -1, 0]]), np.array([[1, 0, 0]])]
     # also hardcoding the human's model for now for development
     # with open('models/augmented_taxi/wt_vi_traj_candidates_human.pickle', 'rb') as f:
     #     wt_vi_traj_candidates_human = pickle.load(f)
@@ -272,7 +272,8 @@ def obtain_summary_counterfactual(summary_variant, wt_vi_traj_candidates, min_BE
             # code to run through and recalculate the human's optimal policy (given new weights)
             wt_vi_traj_candidates_human = copy.deepcopy(wt_vi_traj_candidates)
             for idx, candidate in enumerate(wt_vi_traj_candidates_human):
-                print(idx)
+                if idx % 100 == 0:
+                    print(idx)
                 mdp = candidate[0][1].mdp
                 mdp.weights = w_human_normalized
                 vi_human = ValueIteration(mdp, sample_rate=1, max_iterations=50)
@@ -415,6 +416,7 @@ def obtain_summary_counterfactual(summary_variant, wt_vi_traj_candidates, min_BE
         best_human_trajs = human_counterfactual_trajs_select_model[best_idx]
         best_mdp = wt_vi_traj_candidates[best_env_idx][0][1].mdp
         min_BEC_constraints_running.extend(constraints_select_model[best_idx])
+        min_BEC_constraints_running = BEC_helpers.remove_redundant_constraints(min_BEC_constraints_running, weights, step_cost_flag)[0]
         summary.append([best_mdp, best_traj, constraints_select_model[best_idx], best_human_trajs])
 
         del traj_record[best_idx]
@@ -709,7 +711,7 @@ def obtain_summary(summary_variant, wt_vi_traj_candidates, min_BEC_constraints, 
         summary.reverse()
     return summary
 
-def visualize_constraints(constraints, weights, step_cost_flag, plot_lim=[(-1, 1), (-1, 1)], scale=1.0, fig_name=None):
+def visualize_constraints(constraints, weights, step_cost_flag, plot_lim=[(-1, 1), (-1, 1)], scale=1.0, fig_name=None, just_save=False):
     '''
     Summary: Visualize the constraints. Use scale to determine whether to show L1 normalized weights and constraints or
     weights and constraints where the step cost is 1.
@@ -787,7 +789,8 @@ def visualize_constraints(constraints, weights, step_cost_flag, plot_lim=[(-1, 1
     plt.tight_layout()
     if fig_name is not None:
         plt.savefig(fig_name, dpi=200, transparent=True)
-    plt.show()
+    if not just_save:
+        plt.show()
 
 
 def visualize_summary(BEC_summaries_collection, weights, step_cost_flag):

policy_summarization/BEC_helpers.py
@@ -291,7 +291,7 @@ def obtain_extreme_vertices(constraints, weights, step_cost_flag):
         unique_vertex = unique_vertices[idx, :]
         # back out the step cost using the L1 constraint and add it if called for
         if step_cost_flag:
-            unique_vertex = np.append(unique_vertex, 1 - np.sum(abs(unique_vertices[0, :])))
+            unique_vertex = np.append(unique_vertex, -(1 - np.sum(abs(unique_vertices[0, :])))) # negative since it's a cost
         unique_extreme_vertices.append(unique_vertex.reshape(1, -1))
 
     return unique_extreme_vertices

simple_rl/tasks/skateboard/SkateboardOOMDPClass.py
@@ -52,9 +52,10 @@ class SkateboardOOMDP(OOMDP):
         self.slip_prob = slip_prob
 
         init_state = self._create_state(agent_obj, skateboard_objs)
-        self.exit_state = self._create_state(OOMDPObject(attributes=agent_exit, name="agent_exit"), skateboard_objs_exit)
-        self.exit_state.set_terminal(True)
-        self.exit_state.set_goal(False)
+        # create a reference exit state
+        self.ref_exit_state = self._create_state(OOMDPObject(attributes=agent_exit, name="agent_exit"), skateboard_objs_exit)
+        self.ref_exit_state.set_terminal(True)
+        self.ref_exit_state.set_goal(False)
         OOMDP.__init__(self, SkateboardOOMDP.ACTIONS, self._skateboard_transition_func, self._skateboard_reward_func,
                        init_state=init_state, gamma=gamma, step_cost=step_cost, sample_rate=sample_rate)
 
@@ -110,7 +111,7 @@ class SkateboardOOMDP(OOMDP):
 
         agent = state.get_first_obj_of_class("agent")
 
-        if next_state == self.exit_state:
+        if next_state.is_an_exit_state(self.ref_exit_state):
             return np.array([[0, 0, 0]])
 
         # movement is penalized differently based on whether you have the skateboard or not
@@ -159,7 +160,7 @@ class SkateboardOOMDP(OOMDP):
         '''
         _error_check(state, action)
 
-        state_is_terminal, state_is_goal = skateboard_helpers.is_terminal_and_goal_state(self, state, self.exit_state)
+        state_is_terminal, state_is_goal = skateboard_helpers.is_terminal_and_goal_state(self, state, self.ref_exit_state)
         if not state_is_terminal:
             # if there is a slip, prevent a navigation action from occurring
             stuck = False
@@ -175,9 +176,11 @@ class SkateboardOOMDP(OOMDP):
             elif action == "left" and state.get_agent_x() > 1 and not stuck:
                 next_state = self.move_agent(state, dx=-1)
             elif action == "exit":
-                next_state = copy.deepcopy(self.exit_state)
+                next_state = copy.deepcopy(self.ref_exit_state)
 
-                # ensure that uneaten cookies stay put when the agent exits
+                # ensure that the skateboard is in the same place when the agent exits (note that this is why you can't
+                # simply check whether a state == self.ref_exit_state, since the skateboard can be in different locations
+                # i.e. there are multiple possible exit states)
                 next_state.objects['skateboard'] = state.objects['skateboard'].copy()
             elif action == "dropoff":
                 next_state = self.agent_dropoff(state)
@@ -187,7 +190,7 @@ class SkateboardOOMDP(OOMDP):
                 next_state = state
 
             # Make terminal.
-            next_state_is_terminal, next_state_is_goal = skateboard_helpers.is_terminal_and_goal_state(self, next_state, self.exit_state)
+            next_state_is_terminal, next_state_is_goal = skateboard_helpers.is_terminal_and_goal_state(self, next_state, self.ref_exit_state)
             if next_state_is_terminal:
                 next_state.set_terminal(True)
             if next_state_is_goal: