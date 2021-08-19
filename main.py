Fix bug in solid angle calculation (all redundant constraints must be removed for accurate calculation). Return counterfactual human trajectories along with tests demonstrations.

main.py
@@ -167,7 +167,7 @@ def obtain_test_environments(mdp_class, data_loc, mdp_parameters, weights, BEC_p
     # use generalized area of the BEC region to select test environments
     try:
         with open('models/' + data_loc + '/test_environments.pickle', 'rb') as f:
-            test_wt_vi_traj_tuples, test_BEC_lengths, test_BEC_constraints = pickle.load(f)
+            test_wt_vi_traj_tuples, test_BEC_lengths, test_BEC_constraints, test_human_counterfactual_trajs = pickle.load(f)
 
     except:
         ps_helpers.obtain_env_policies(mdp_class, data_loc, np.expand_dims(weights, axis=0), mdp_parameters, pool)
@@ -235,7 +235,7 @@ def obtain_test_environments(mdp_class, data_loc, mdp_parameters, weights, BEC_p
             else:
                 args = [(i, n_human_models, prior, posterior, data_loc, -1, weights, traj_record[i], step_cost_flag, pool) for i in range(len(env_record))]
 
-            print("Obtaining overlap in BEC area between posterior human model and potential test demosntrations: ")
+            print("Obtaining overlap in BEC area between posterior human model and potential test demonstrations: ")
             min_subset_constraints_record_counterfactual, BEC_lengths_record_counterfactual, overlap_in_opt_and_counterfactual_traj_avg, human_counterfactual_trajs = zip(*pool.imap(BEC.combine_limiting_constraints_BEC, tqdm(args)))
             pool.close()
             pool.join()
@@ -245,20 +245,20 @@ def obtain_test_environments(mdp_class, data_loc, mdp_parameters, weights, BEC_p
                 pickle.dump((min_subset_constraints_record_counterfactual, BEC_lengths_record_counterfactual, overlap_in_opt_and_counterfactual_traj_avg, human_counterfactual_trajs), f)
 
         if use_counterfactual:
-            test_wt_vi_traj_tuples, test_BEC_lengths, test_BEC_constraints = \
-                ps_helpers.obtain_test_environments(data_loc, min_subset_constraints_record_counterfactual, env_record, traj_record, weights, BEC_lengths_record_counterfactual, BEC_params['n_test_demos'], BEC_params['test_difficulty'], step_cost_flag, summary=summary)
+            test_wt_vi_traj_tuples, test_BEC_lengths, test_BEC_constraints, test_human_counterfactual_trajs = \
+                ps_helpers.obtain_test_environments(data_loc, min_subset_constraints_record_counterfactual, env_record, traj_record, weights, BEC_lengths_record_counterfactual, BEC_params['n_test_demos'], BEC_params['test_difficulty'], step_cost_flag, summary=summary, human_counterfactual_trajs=human_counterfactual_trajs)
         else:
-            test_wt_vi_traj_tuples, test_BEC_lengths, test_BEC_constraints = \
+            test_wt_vi_traj_tuples, test_BEC_lengths, test_BEC_constraints, test_human_counterfactual_trajs = \
                 ps_helpers.obtain_test_environments(data_loc, min_subset_constraints_record, env_record,
                                                     traj_record, weights, BEC_lengths_record,
                                                     BEC_params['n_test_demos'], BEC_params['test_difficulty'],
                                                     step_cost_flag, summary=summary)
 
         with open('models/' + data_loc + '/test_environments.pickle', 'wb') as f:
-            pickle.dump((test_wt_vi_traj_tuples, test_BEC_lengths, test_BEC_constraints), f)
+            pickle.dump((test_wt_vi_traj_tuples, test_BEC_lengths, test_BEC_constraints, test_human_counterfactual_trajs), f)
 
     if visualize_test_env:
-        BEC.visualize_test_envs(posterior, test_wt_vi_traj_tuples, test_BEC_lengths, test_BEC_constraints, weights, step_cost_flag)
+        BEC.visualize_test_envs(posterior, test_wt_vi_traj_tuples, test_BEC_lengths, test_BEC_constraints, test_human_counterfactual_trajs, weights, step_cost_flag)
     return test_wt_vi_traj_tuples, test_BEC_lengths, test_BEC_constraints
 
 if __name__ == "__main__":

policy_summarization/BEC.py
@@ -342,7 +342,7 @@ def compute_counterfactuals(args):
                         skip_demo = True
 
             if not skip_demo:
-                info_gain = BEC_helpers.calculate_information_gain(min_BEC_constraints_running, constraints)
+                info_gain = BEC_helpers.calculate_information_gain(min_BEC_constraints_running, constraints, weights, step_cost_flag)
             else:
                 info_gain = 0
 
@@ -414,7 +414,7 @@ def combine_limiting_constraints_IG(args):
                     skip_demo = True
 
         if not skip_demo:
-            ig = BEC_helpers.calculate_information_gain(min_BEC_constraints_running, min_env_constraints)
+            ig = BEC_helpers.calculate_information_gain(min_BEC_constraints_running, min_env_constraints, weights, step_cost_flag)
             info_gains_record.append(ig)
         else:
             info_gains_record.append(0)
@@ -504,28 +504,36 @@ def combine_limiting_constraints_BEC(args):
     # for each possible demonstration in each environment, find the overlap in area between the spherical polygon
     # comprising the posterior and the counterfactual constraints created by the demonstration
     for traj_idx in range(len(all_env_constraints_joint)):
-        all_env_constraints_joint_traj = all_env_constraints_joint[traj_idx]
-        all_env_constraints_joint_traj = BEC_helpers.remove_redundant_constraints(all_env_constraints_joint_traj, weights, step_cost_flag)
+        all_env_constraints_joint_traj = all_env_constraints_joint[traj_idx].copy()
 
-        traj_ieqs = BEC_helpers.constraints_to_halfspace_matrix_sage(all_env_constraints_joint_traj)
-        traj_poly = Polyhedron.Polyhedron(ieqs=traj_ieqs)  # automatically finds the minimal H-representation
-        traj_spherical_polygon_vertices = np.array(BEC_helpers.obtain_sph_polygon_vertices(traj_poly, add_noise=True)) # add noise to help smooth calculation
-
-        inside_traj_sph_polygon = BEC_helpers.sample_average_model(all_env_constraints_joint_traj, sample_rate=100)[0][0]
-
-        traj_spherical_polygon_vertices = cg.sort_points_by_angle(traj_spherical_polygon_vertices, inside_traj_sph_polygon)
-        traj_sph_polygon = sph_polygon.SphericalPolygon(traj_spherical_polygon_vertices, inside=tuple(inside_traj_sph_polygon))
-
-        # if abs(BEC_helpers.calc_solid_angles([all_env_constraints_joint_traj])[0] - traj_sph_polygon.area()) > 1  or np.isnan(traj_sph_polygon.area()):
-        #     print('env: {}, traj: {}'.format(env_idx, traj_idx))
-        #     raise AssertionError("Too much deviation in trajectory BEC areas")
-
-        if traj_sph_polygon.intersects_poly(posterior_sph_polygon):
-            BEC_areas.append(traj_sph_polygon.intersection(posterior_sph_polygon).area())
+        if len(all_env_constraints_joint_traj) == 0:
+            BEC_areas.append(posterior_sph_polygon.area())
         else:
-            BEC_areas.append(0)
+            if len(all_env_constraints_joint_traj) > 1:
+                all_env_constraints_joint_traj = BEC_helpers.remove_redundant_constraints(all_env_constraints_joint_traj,
+                                                                                          weights, step_cost_flag)
 
-        min_env_constraints_record.append(all_env_constraints_joint[traj_idx])
+            traj_ieqs = BEC_helpers.constraints_to_halfspace_matrix_sage(all_env_constraints_joint_traj)
+            traj_poly = Polyhedron.Polyhedron(ieqs=traj_ieqs)  # automatically finds the minimal H-representation
+            traj_spherical_polygon_vertices = np.array(BEC_helpers.obtain_sph_polygon_vertices(traj_poly, add_noise=True)) # add noise to help smooth calculation
+
+            inside_traj_sph_polygon = BEC_helpers.sample_average_model(all_env_constraints_joint_traj, sample_rate=100)[0][0]
+
+            traj_spherical_polygon_vertices = cg.sort_points_by_angle(traj_spherical_polygon_vertices, inside_traj_sph_polygon)
+            traj_sph_polygon = sph_polygon.SphericalPolygon(traj_spherical_polygon_vertices, inside=tuple(inside_traj_sph_polygon))
+
+            # if abs(BEC_helpers.calc_solid_angles([all_env_constraints_joint_traj])[0] - traj_sph_polygon.area()) > 1  or np.isnan(traj_sph_polygon.area()):
+            #     print('env: {}, traj: {}'.format(env_idx, traj_idx))
+            #     raise AssertionError("Too much deviation in trajectory BEC areas")
+
+            if traj_sph_polygon.intersects_poly(posterior_sph_polygon):
+                BEC_areas.append(traj_sph_polygon.intersection(posterior_sph_polygon).area())
+                # intersection_area = traj_sph_polygon.intersection(posterior_sph_polygon).area()
+                # BEC_areas.append(intersection_area / (posterior_sph_polygon.area() - intersection_area))
+            else:
+                BEC_areas.append(0)
+
+        min_env_constraints_record.append(all_env_constraints_joint_traj)
 
     # obtain the counterfactual human trajectories that could've given rise to the most limiting constraints and
     # how much it overlaps the agent's optimal trajectory
@@ -657,12 +665,12 @@ def obtain_summary_counterfactual(data_loc, summary_variant, min_BEC_constraints
         no_info_flag = False
         if consistent_state_count:
             info_gains = np.array(info_gains_record)
-            if np.sum(info_gains) == 0:
+            if np.sum(info_gains > 1) == 0:
                 no_info_flag = True
         else:
             info_gains_flattened_across_models = list(itertools.chain.from_iterable(info_gains_record))
             info_gains_flattened_across_envs = list(itertools.chain.from_iterable(info_gains_flattened_across_models))
-            if sum(info_gains_flattened_across_envs) == 0:
+            if sum(np.array(info_gains_flattened_across_envs) > 1) == 0:
                 no_info_flag = True
 
         # no need to continue search for demonstrations if none of them will improve the human's understanding
@@ -698,11 +706,11 @@ def obtain_summary_counterfactual(data_loc, summary_variant, min_BEC_constraints
             no_info_flag = False
             if consistent_state_count:
                 info_gains = np.array(info_gains_record)
-                if np.sum(info_gains) == 0:
+                if np.sum(info_gains > 1) == 0:
                     no_info_flag = True
             else:
                 info_gains_flattened_across_trajs = list(itertools.chain.from_iterable(info_gains_record))
-                if sum(info_gains_flattened_across_trajs) == 0:
+                if sum(np.array(info_gains_flattened_across_trajs) > 1) == 0:
                     no_info_flag = True
 
             if no_info_flag:

policy_summarization/BEC_helpers.py
@@ -706,10 +706,11 @@ def selectKcities(n, weights, k):
 
     return centers
 
-def calculate_information_gain(previous_constraints, new_constraints):
+def calculate_information_gain(previous_constraints, new_constraints, weights, step_cost_flag):
     if len(previous_constraints) > 0 and len(new_constraints) > 0:
         hypothetical_constraints = new_constraints.copy()
         hypothetical_constraints.extend(previous_constraints)
+        hypothetical_constraints = remove_redundant_constraints(hypothetical_constraints, weights, step_cost_flag)
 
         old_BEC = calc_solid_angles([previous_constraints])[0]
         new_BEC = calc_solid_angles([hypothetical_constraints])[0]
@@ -717,7 +718,7 @@ def calculate_information_gain(previous_constraints, new_constraints):
         ig = old_BEC / new_BEC
     elif len(new_constraints) > 0:
         old_BEC = 4 * np.pi
-        new_BEC = calc_solid_angles([previous_constraints])[0]
+        new_BEC = calc_solid_angles([new_constraints])[0]
 
         ig = old_BEC / new_BEC
     else:

policy_summarization/policy_summarization_helpers.py
@@ -219,7 +219,7 @@ def _in_summary(mdp, summary, initial_state):
             return True
     return False
 
-def select_test_demos(cluster_idx, data_loc, n_desired_test_env, env_idxs , traj_opts, BEC_lengths, BEC_constraints, n_clusters=6):
+def select_test_demos(cluster_idx, data_loc, n_desired_test_env, env_idxs , traj_opts, BEC_lengths, BEC_constraints, human_counterfactual_trajs, n_clusters=6):
     kmeans = KMeans(n_clusters=n_clusters).fit(np.array(BEC_lengths).reshape(-1, 1))
     cluster_centers = kmeans.cluster_centers_
     labels = kmeans.labels_
@@ -255,8 +255,12 @@ def select_test_demos(cluster_idx, data_loc, n_desired_test_env, env_idxs , traj
 
     test_BEC_lengths = [BEC_lengths[k] for k in test_demo_idxs]
     test_BEC_constraints = [BEC_constraints[k] for k in test_demo_idxs]
+    if human_counterfactual_trajs is not None:
+        test_human_counterfactual_trajs = [human_counterfactual_trajs[k] for k in test_demo_idxs]
+    else:
+        test_human_counterfactual_trajs = None
 
-    return test_wt_vi_traj_tuples, test_BEC_lengths, test_BEC_constraints
+    return test_wt_vi_traj_tuples, test_BEC_lengths, test_BEC_constraints, test_human_counterfactual_trajs
 
 
 def optimize_visuals(data_loc, best_env_idxs, best_traj_idxs, chunked_traj_record, summary):
@@ -298,7 +302,7 @@ def optimize_visuals(data_loc, best_env_idxs, best_traj_idxs, chunked_traj_recor
 
     return best_env_idx, best_traj_idx
 
-def obtain_test_environments(data_loc, min_subset_constraints_record, env_record, traj_record, weights, BEC_lengths_record, n_desired_test_env, difficulty, step_cost_flag, summary=None, overlap_in_trajs_avg=None, c=0.001):
+def obtain_test_environments(data_loc, min_subset_constraints_record, env_record, traj_record, weights, BEC_lengths_record, n_desired_test_env, difficulty, step_cost_flag, summary=None, overlap_in_trajs_avg=None, c=0.001, human_counterfactual_trajs=None):
     '''
     Summary: Correlate the difficulty of a test environment with the generalized area of the BEC region obtain by the
     corresponding optimal demonstration. Return the desired number and difficulty of test environments (to be given
@@ -326,6 +330,8 @@ def obtain_test_environments(data_loc, min_subset_constraints_record, env_record
             del BEC_lengths_record_filtered[env_idx][traj_idx]
             if overlap_in_trajs_avg is not None:
                 del overlap_in_trajs_avg_filtered[env_idx][traj_idx]
+            if human_counterfactual_trajs is not None:
+                del human_counterfactual_trajs[env_idx][traj_idx]
 
     # flatten relevant lists for easy sorting
     envs_record_flattened = []
@@ -334,6 +340,8 @@ def obtain_test_environments(data_loc, min_subset_constraints_record, env_record
 
     traj_record_flattened = [item for sublist in traj_record_filtered for item in sublist]
     min_subset_constraints_record_flattened = [item for sublist in min_subset_constraints_record_filtered for item in sublist]
+    human_counterfactual_trajs_flattened = [item for sublist in human_counterfactual_trajs for item in sublist]
+
     if overlap_in_trajs_avg is None:
         obj_func_flattened = [item for sublist in BEC_lengths_record_filtered for item in sublist]
     else:
@@ -342,17 +350,17 @@ def obtain_test_environments(data_loc, min_subset_constraints_record, env_record
         obj_func_flattened = np.array(BEC_lengths_record_flattened) * (np.array(overlap_in_trajs_avg_flattened) + c)
 
     if difficulty == 'high':
-        test_wt_vi_traj_tuples, test_BEC_lengths, test_BEC_constraints = select_test_demos(0, data_loc, n_desired_test_env, envs_record_flattened, traj_record_flattened, obj_func_flattened, min_subset_constraints_record_flattened)
+        test_wt_vi_traj_tuples, test_BEC_lengths, test_BEC_constraints, test_human_counterfactual_trajs = select_test_demos(0, data_loc, n_desired_test_env, envs_record_flattened, traj_record_flattened, obj_func_flattened, min_subset_constraints_record_flattened, human_counterfactual_trajs_flattened)
     if difficulty == 'medium':
-        test_wt_vi_traj_tuples, test_BEC_lengths, test_BEC_constraints = select_test_demos(2, data_loc, n_desired_test_env,
+        test_wt_vi_traj_tuples, test_BEC_lengths, test_BEC_constraints, test_human_counterfactual_trajs = select_test_demos(2, data_loc, n_desired_test_env,
                                                                                            envs_record_flattened,
                                                                                            traj_record_flattened,
-                                                                                           obj_func_flattened, min_subset_constraints_record_flattened)
+                                                                                           obj_func_flattened, min_subset_constraints_record_flattened, human_counterfactual_trajs_flattened)
 
     if difficulty == 'low':
-        test_wt_vi_traj_tuples, test_BEC_lengths, test_BEC_constraints = select_test_demos(4, data_loc, n_desired_test_env,
+        test_wt_vi_traj_tuples, test_BEC_lengths, test_BEC_constraints, test_human_counterfactual_trajs = select_test_demos(4, data_loc, n_desired_test_env,
                                                                                            envs_record_flattened,
                                                                                            traj_record_flattened,
-                                                                                           obj_func_flattened, min_subset_constraints_record_flattened)
+                                                                                           obj_func_flattened, min_subset_constraints_record_flattened, human_counterfactual_trajs_flattened)
 
-    return test_wt_vi_traj_tuples, test_BEC_lengths, test_BEC_constraints
+    return test_wt_vi_traj_tuples, test_BEC_lengths, test_BEC_constraints, test_human_counterfactual_trajs