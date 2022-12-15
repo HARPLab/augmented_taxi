Fix bug in visual dissimilarity calculation for taxi and skateboard environments.

simple_rl/tasks/skateboard/Skateboard2OOMDPClass.py
@@ -368,7 +368,7 @@ class Skateboard2OOMDP(OOMDP):
         overlap = min(len(start_state_hash), len(other_start_state_hash))
 
         dissimilarity += np.sum(np.abs(np.array([int(x) for x in start_state_hash[0:overlap]]) - np.array(
-            [int(x) for x in other_start_state[0:overlap]]))) * start_state_weight
+            [int(x) for x in other_start_state_hash[0:overlap]]))) * start_state_weight
 
         # paths and skateboard
         dissimilarity += np.sum(np.abs(np.array(self.env_code) - np.array(other_mdp.env_code)))