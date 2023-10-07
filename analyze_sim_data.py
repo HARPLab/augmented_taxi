simulation-ready code. Fixed knowledge level calculation

analyze_sim_data.py
@@ -25,16 +25,22 @@ def str_to_dict(string, var_type = None, splitter = ', '):
     # the dictionary, converting the values to
     # integers and removing the quotes from the keys
     dict = {}
-    print(pairs)
+    # print(pairs)
     for i in range(len(pairs)):
         pair = pairs[i]
         key, value = pair.split(': ')
+        
+        
         key = key.strip(' \' ')
+        # print('key: ', key)
+        # print('value: ', value)
 
         if splitter == ', ':
             value = value.strip(' \' ')  # strip is used only for leading and trailing characters
-            value = value.strip(' array ')
-            value = value.strip(' ([]) ')
+            value = value.strip('[]')
+            value = value.strip('array')
+            value = value.strip('([])')
+            # print('value 1: ', value)
         else:
             value = value.replace('array', '')
             value = value.replace('(', '')
@@ -44,6 +50,7 @@ def str_to_dict(string, var_type = None, splitter = ', '):
             # print('Check before: ', value)
             # value = ast.literal_eval(value)
             value = eval(value)
+            # print('value 2: ', value)
             if key == 'joint_knowledge':
                 value_copy = []
                 for val in value:
@@ -54,8 +61,8 @@ def str_to_dict(string, var_type = None, splitter = ', '):
                 value = value_copy
             else:   
                 value = [np.array(v) for v in value]
-            # print('Type after: ', type(value))
-            # print('Check after: ', value)
+            print('Type after: ', type(value))
+            print('Check after: ', value)
         
         if var_type == 'float':
             dict[key] = float(value)
@@ -68,11 +75,18 @@ def str_to_dict(string, var_type = None, splitter = ', '):
     return dict
  
 
+def normalize_knowledge(knowledge):
+    knowledge['loop_count_normalized'] = np.zeros(len(knowledge))
+    for i in range(len(knowledge)):
+        knowledge['loop_count_normalized'][i] = knowledge['loop_count'][i]/max(knowledge['loop_count'])
+
+    return knowledge
+
 
 
 if __name__ == "__main__":
     # params = Params('params_taxi.json')
-    sim_vars = pd.read_csv('models/augmented_taxi2/hlm_.csv')
+    sim_vars = pd.read_csv('models/augmented_taxi2/new_hlm_sim_run_1 - Copy.csv')
     # print(len(sim_vars))
 
     team_unit_knowledge_level = pd.DataFrame()
@@ -97,6 +111,9 @@ if __name__ == "__main__":
         BEC_team_knowledge_dict['loop_count'] = int(sim_vars['loop_count'][i])
         BEC_team_knowledge_dict['demo_strategy'] = sim_vars['demo_strategy'][i]
         team_BEC_knowledge_level = team_BEC_knowledge_level.append(BEC_team_knowledge_dict, ignore_index=True)
+        
+        team_BEC_knowledge_level_normalized = normalize_knowledge(team_BEC_knowledge_level)
+
 
         # team knowledge constraints
         tkc = sim_vars['team_knowledge'][i]
@@ -129,10 +146,15 @@ if __name__ == "__main__":
     # print(team_knowledge)
 
 
-    for know_id in ['p1', 'p2', 'p3', 'common_knowledge', 'joint_knowledge']:
-        sns.lineplot(data = team_BEC_knowledge_level, x = 'loop_count', y = know_id, hue = 'demo_strategy').set(title='Knowledge level for ' + know_id)
-        plt.show()
-        plt.savefig('models/augmented_taxi2/BEC_knowledge_level_' + know_id + '.png')
-        plt.close()
+    # for know_id in ['p1', 'p2', 'p3', 'common_knowledge', 'joint_knowledge']:
+    #     sns.lineplot(data = team_BEC_knowledge_level, x = 'loop_count', y = know_id, hue = 'demo_strategy').set(title='Knowledge level for ' + know_id)
+    #     plt.show()
+    #     # plt.savefig('models/augmented_taxi2/BEC_knowledge_level_' + know_id + '.png')
+    #     # plt.close()
+
+    sea = sns.FacetGrid(team_BEC_knowledge_level_normalized, row = "loop_count", col="know_id", hue="demo_strategy", col_wrap=2)
+
+    sea.map(sns.lineplot, "loop_count", "knowledge_level", color='red')
+
 
     

params_team.py
@@ -342,17 +342,18 @@ test_env_traj_tracers = {
 
 ### Added for robot teaching to team situation
 
-team_size = 1
+team_size = 3
 
-human_learning_models = {'p1': np.array([0.9, 0.9, 0.9]), \
-                         'p2': np.array([0.8, 0.8, 0.8]), \
-                         'p3': np.array([0.7, 0.7, 0.7]) }
+
+# human_learning_models = {'p1': np.array([0.9, 0.9, 0.9]), \
+#                          'p2': np.array([0.8, 0.8, 0.8]), \
+#                          'p3': np.array([0.7, 0.7, 0.7]) }
 
 
 team_prior = {}
 for i in range(team_size):
     member_id = 'p' + str(i+1)
-    team_prior[member_id] = prior
+    team_prior[member_id] = [prior]
 
 
 
@@ -360,12 +361,14 @@ knowledge = 'common_knowledge'
 # knowledge = 'joint_knowledge'
 # knowledge = 'individual_knowledge'
 
-learning_goal = {'common_knowledge': np.array([0.5, 0.5, 0.5]), \
-                  'joint_knowledge': np.array([0.95, 0.95, 0.95]), \
-                  'individual_knowledge': np.array([0.7, 0.7, 0.7])}
+# learning_goal = {'common_knowledge': np.array([0.5, 0.5, 0.5]), \
+#                   'joint_knowledge': np.array([0.95, 0.95, 0.95]), \
+#                   'individual_knowledge': np.array([0.7, 0.7, 0.7])}
 
 demo_strategy = 'common_knowledge'
 # demo_strategy = 'joint_knowledge'
 # demo_strategy = 'individual_knowledge_low'
 # demo_strategy = 'individual_knowledge_high'
 
+max_KC_loops = 10
+learning_goal = 1.0

run_simulations.py
@@ -14,6 +14,7 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 import os
 import itertools
+from itertools import permutations
 
 # Other imports.
 sys.path.append("simple_rl")
@@ -47,7 +48,7 @@ if __name__ == "__main__":
     os.makedirs('models/' + params.data_loc['base'], exist_ok=True)
     os.makedirs('models/' + params.data_loc['BEC'], exist_ok=True)
 
-    N_runs = 1
+    N_runs = 100
 
     # weights_list = [[0.6, 0.15, 0.25], [0.5, 0.25, 0.25], [0.4, 0.3, 0.3], [0.8, 0.05, 0.1], [0.2, 0.6, 0.2], [0.2, 0.2, 0.6], [0.5, 0.1, 0.4], [0, 0, 1], [1, 0, 0], [0, 1, 0]]
     
@@ -78,14 +79,51 @@ if __name__ == "__main__":
     #     # vars_to_save = run_reward_teaching(params, pool, demo_strategy = dem_strategy, response_type = 'simulated', response_distribution_list= resp_dist_list, run_no = i, vars_to_save = vars_to_save)
     #     run_reward_teaching(params, pool, demo_strategy = dem_strategy, response_type = 'simulated', viz_flag=[False, False, False], response_distribution_list= resp_dist_list, run_no = i, vars_filename='sim_run_mixed_maj')
 
+    sim_params = {'min_correct_likelihood': 0.5}
+    start_id = 20
 
-    for i in range(1, N_runs+1):
-        ilcr = random.random()*0.2 + 0.5 # between 0.5 and 0.7 
-        rlcr = random.random()*0.10 + 0.05 # between 0.05 and 0.15
+
+    team_params_learning = {'low': [0.5, 0.05], 'med': [0.6, 0.07], 'high': [0.7, 0.1]}
+    print([*range(params.team_size)])
+    # team_composition_list = list(permutations([*range(params.team_size)]))
+    team_composition_list = [[0,1,2], [0,2,1], [1,0,2], [1,2,0], [2,0,1], [2,1,0]]
+    print(team_composition_list)
+    for i in range(50, N_runs+1):
+        team_composition_for_run = team_composition_list[np.random.choice(range(len(team_composition_list)))]
+        print('team_composition_for_run: ', team_composition_for_run)
+
+        ilcr = np.zeros(params.team_size)
+        rlcr = np.zeros([params.team_size, 2])
+
+        for j in range(params.team_size):
+            if team_composition_for_run[j] == 0:
+                ilcr[j] = random.random()*0.1 + team_params_learning['low'][0]  # between 0.5 and 0.6
+                x = random.random()*0.05 + team_params_learning['low'][1]  # between 0.05 and 0.15
+            elif team_composition_for_run[j] == 1:
+                ilcr[j] = random.random()*0.1 + team_params_learning['med'][0]  # between 0.5 and 0.6
+                x = random.random()*0.05 + team_params_learning['med'][1]  # between 0.05 and 0.15
+            elif team_composition_for_run[j] == 2:
+                ilcr[j] = random.random()*0.1 + team_params_learning['high'][0]  # between 0.5 and 0.6
+                x = random.random()*0.05 + team_params_learning['high'][1]  # between 0.05 and 0.15
+                
+            rlcr[j,0] = x
+            rlcr[j,1] = -x      
+            
+        
         # dem_strategy = random.choice(['common_knowledge', 'joint_knowledge', 'individual_knowledge_low', 'individual_knowledge_high'])
-        dem_strategy = 'individual_knowledge_low'
-        print('Simulation run: ', i, '. Demo strategy: ', dem_strategy, 'initial lcr: ', ilcr, 'rate lcr: ', rlcr)
-        run_reward_teaching(params, pool, demo_strategy = 'common_knowledge', experiment_type = 'simulated', team_likelihood_correct_response = ilcr*np.ones(params.team_size) ,  team_learning_rate = np.hstack((rlcr*np.ones([params.team_size, 1]), -rlcr*np.ones([params.team_size, 1]))), viz_flag=[True, True, True], run_no = i, vars_filename='hlm_sim_3')
+        dem_strategy_list = ['individual_knowledge_low', 'individual_knowledge_high', 'common_knowledge', 'joint_knowledge']
+        
+        if (i-start_id)/(N_runs-start_id) < 0.25:
+            dem_strategy = dem_strategy_list[0]
+        elif (i-start_id)/(N_runs-start_id) < 0.5:
+            dem_strategy = dem_strategy_list[1]
+        elif (i-start_id)/(N_runs-start_id) < 0.75:
+            dem_strategy = dem_strategy_list[2]
+        else:
+            dem_strategy = dem_strategy_list[3]
+
+        print(colored('Simulation run: ' + str(i) + '. Demo strategy: ' + str(dem_strategy) + 'initial lcr: ' + str(ilcr) + 'rate lcr: ' + str(rlcr), 'red'))
+        run_reward_teaching(params, pool, sim_params, demo_strategy = dem_strategy, experiment_type = 'simulated', team_likelihood_correct_response = ilcr,  team_learning_rate = rlcr, viz_flag=[False, False, False], run_no = i, vars_filename='new_hlm_sim_run_2')
 
 
     # # save all the variables

teams/particle_filter_team.py
@@ -35,6 +35,8 @@ class Particles_team():
         self.positions_prev = self.positions.copy()
         self.weights_prev = self.weights.copy()
 
+        self.constraints = []
+
         # Spherical discretization taken from : A new method to subdivide a spherical surface into equal-area cells,
         # Malkin 2019 https://arxiv.org/pdf/1612.03467.pdf
         self.ele_bin_edges = np.array([0, 0.1721331, 0.34555774, 0.52165622, 0.69434434, 0.86288729,
@@ -746,7 +748,13 @@ class Particles_team():
 
         print(colored('Performed a reset', 'red'))
         self.pf_reset_count += 1
-        self.reset_constraint = constraint
+        self.reset_constraint = [constraint[0]]
+
+    
+    # currently unused - will be needed when particles are reset based on all prior knowledge instead of just the last constraint
+    def knowledge_update(self, constraints):
+        self.constraints = copy.deepcopy(constraints)
+
 
 
     def update(self, constraints, c=0.5, reset_threshold_prob=0.001):

teams/utils_teams.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+import itertools
 
 
 def plot_normal(normal, X, Y, Z, ax=None):
@@ -39,7 +40,7 @@ def visualize_planes_team(constraints, fig=None, ax=None, alpha=0.5, color=None)
                 ax.plot_surface(X_xy, Y_xy, Z, alpha=alpha, color=color)
             else:
                 ax.plot_surface(X_xy, Y_xy, Z, alpha=alpha)
-            plot_normal(constraint, X_xy, Y_xy, Z, ax=ax)
+            # plot_normal(constraint, X_xy, Y_xy, Z, ax=ax)
         elif constraint[0, 1] != 0:
             X_xz, Z_xz, = np.meshgrid(x, z)
             Y = (-constraint[0, 0] * X_xz - constraint[0, 2] * Z_xz) / constraint[0, 1]
@@ -47,7 +48,7 @@ def visualize_planes_team(constraints, fig=None, ax=None, alpha=0.5, color=None)
                 ax.plot_surface(X_xz, Y, Z_xz, alpha=alpha, color=color)
             else:
                 ax.plot_surface(X_xz, Y, Z_xz, alpha=alpha)
-            plot_normal(constraint, X_xz, Y, Z_xz, ax=ax)
+            # plot_normal(constraint, X_xz, Y, Z_xz, ax=ax)
         else:
             Y_yz, Z_yz, = np.meshgrid(y, z)
             X = (-constraint[0, 1] * Y_yz - constraint[0, 2] * Z_yz) / constraint[0, 0]
@@ -55,4 +56,8 @@ def visualize_planes_team(constraints, fig=None, ax=None, alpha=0.5, color=None)
                 ax.plot_surface(X, Y_yz, Z_yz, alpha=alpha, color=color)
             else:
                 ax.plot_surface(X, Y_yz, Z_yz, alpha=alpha)
-            plot_normal(constraint, X, Y_yz, Z_yz, ax=ax)
+            # plot_normal(constraint, X, Y_yz, Z_yz, ax=ax)
+
+
+def flatten_list(nested_list):
+    return list(itertools.chain(*nested_list))